3938c712b99f14fbff14c7b9dd5fae8829d7a5c3
two12dev <crob2099@gmail.com>
2021-07-20 14:14:11 -0700

    Created Enemy tests for validating name, HP and min Damage AB#399

---
+++ module/test/com/combat/EnemyTest.java
@@ -0,0 +1,38 @@
+package com.combat;
+
+import com.character.Enemy;
+import com.client.Client;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class EnemyTest {
+
+    @Test
+    public void validateEnemyName(){
+        Enemy bouncer = new Enemy("bouncer");
+        String expected = "bouncer";
+        String actual = bouncer.getName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void validateEnemyHP(){
+        Enemy bouncer = new Enemy("bouncer");
+        int expected = 30;
+        int actual = bouncer.getHp();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void validateEnemyMinDamage(){
+        Enemy bouncer = new Enemy("bouncer");
+        int expected = 5;
+        int actual = bouncer.getMinDamage();
+        assertEquals(expected, actual);
+    }
+
+
+}
