c76f9930ee253f0673e4f6733ea16bb248dfb8b9
Jonathan Urquieta <jonathan.urquieta@tlgcohort.com>
2021-07-20 00:43:13 -0600

    Created the main GUI Screen Layout, AB#377

--- module/src/com/GUI/Controller.java
+++ module/src/com/GUI/Controller.java
@@ -1,13 +1,42 @@
 package com.GUI;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import javax.print.DocFlavor;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class Controller {
-    @FXML
-    public void start (ActionEvent e) {
-        System.out.println(SOTF.stringCombatIntro());
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void switchToMainScreen(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+
+
+
 
 
 }
