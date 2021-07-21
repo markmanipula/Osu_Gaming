a8786b23eb13fd788bb4b51210caab777c5f6f69
Jun Park <jhpp114node@gmail.com>
2021-07-20 21:36:48 -0700

    added the story description to displace based on the player's location, AB#414

--- module/src/com/GUI/maingame/MainScreenController.java
+++ module/src/com/GUI/maingame/MainScreenController.java
@@ -1,12 +1,11 @@
 package com.GUI.maingame;
 
 import com.GUI.SceneController;
+import com.game.Player;
+import com.readjson.ReadRoomContentJson;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +17,27 @@ public class MainScreenController {
     private MenuItem displayMapMenuItem;
     @FXML
     private MenuItem aboutJemad;
+    // script data
+    @FXML
+    private TextArea gameDescription;
+
+    // player obj to retrieve the current location
+    private Player jemad = new Player();
+
+    @FXML
+    public void initialize() {
+        System.out.println("is this print? from initializer");
+        generateDescriptionBasedOnLocation();
+    }
+
+    private void generateDescriptionBasedOnLocation() {
+        // changed the location to inside bar to test
+        jemad.setCurrentLocation("Inside Bar");
+        String currentDescription = ReadRoomContentJson.trimRoomDescription(jemad.getCurrentLocation());
+        gameDescription.setEditable(false);
+        gameDescription.setWrapText(true);
+        gameDescription.setText(currentDescription);
+    }
 
     public void switchItIntoMap(ActionEvent event) throws IOException {
         if (event.getSource() == displayMapMenuItem || event.getSource() == aboutJemad) {
